b77ea4d4ab7c744f80fa32b688c65a03d230b90a
Fwiffo <borgified@gmail.com>
2014-04-23 16:46:34 -0700

    populate employee_ids table

---
+++ employeeIds.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/env perl
+
+use Text::CSV;
+use DBI;
+use Data::Dumper;
+use strict;
+use warnings;
+
+my $file = $ARGV[0] or die "need csv input file\n";
+open(my $data, '<:encoding(utf8)', $file) or die "couldnt open file $!\n";
+
+my %employees;
+my @header;
+
+my $csv = Text::CSV->new ({
+		binary		=> 1,
+		auto_diag	=> 1,
+		sep_char	=> ',',
+	});
+
+
+while(my $fields = $csv->getline($data)){
+
+	#detect if the first field starts with 'Employee Name' if so, then this is
+	#our header we'll use it as the key for our hash.
+	my $x=0;
+
+	if(${$fields}[0] eq 'Employee Name'){
+		@header=@{$fields};
+		next;
+	}else{
+
+		foreach my $field (@{$fields}){
+			$employees{"${$fields}[1]"}{"$header[$x]"}="$field";
+			$x++;
+		}
+
+	}
+}
+
+#print Dumper(\%employees);exit;
+
+my $my_cnf = '/secret/my_cnf.cnf';
+
+my $dbh = DBI->connect("DBI:mysql:"
+	. ";mysql_read_default_file=$my_cnf"
+	.';mysql_read_default_group=ldap',
+	undef,
+	undef
+) or die "something went wrong ($DBI::errstr)";
+
+my $clear_data = $dbh->prepare("truncate table employee_ids");
+$clear_data->execute or die "SQL Error: $DBI::errstr\n";
+
+
+my $query = $dbh->prepare("insert into employee_ids (eeid,email) values (?,?)");
+
+foreach my $eeid (keys %employees){
+	my $email = $employees{$eeid}{'Work Email'};
+	#validate employee id and actian email
+	if (($eeid =~ /^\d+$/) && ($email =~ /[\w\-]+\.[\w\-]+\@actian\.com/)){
+		$query->execute($eeid, $employees{$eeid}{'Work Email'}) or die "SQL Error: $DBI::errstr\n";
+	}else{
+		print "employee id ($eeid) or email ($email) problem\n";
+	}
+}
